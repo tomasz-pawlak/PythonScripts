2a427d16c6a62f5e8e9008491ea4a4a3066476ac
gurtoc <gurtoc@gmail.com>
2025-04-19 10:49:06 +0200

    + Added def to generate output file report

--- virustotal_checker/virustotal_checker.py
+++ virustotal_checker/virustotal_checker.py
@@ -10,6 +10,16 @@ def get_hash_from_file(file_path, hash_type):
     with open(file_path, "rb") as f:
         for byte_block in iter(lambda: f.read(4096), b""):
             hash_func.update(byte_block)
+
+    if args.report:
+        try:
+            print("generating report to file...")
+            generate_report(hash_func.hexdigest())
+        except Exception as e:
+            pass
+
+    print("-----------------------------")
+    print(str(hash_func.hexdigest()))
     return hash_func.hexdigest()
 
 
@@ -23,7 +33,24 @@ def get_hash_from_url(url, hash_type):
     for byte_block in response.iter_content(4096):
         hash_func.update(byte_block)
 
+    if args.report:
+        try:
+            print("generating report to file...")
+            generate_report(hash_func.hexdigest())
+        except Exception as e:
+            pass
+
     return hash_func.hexdigest()
+
+
+def generate_report(hash_calculated):
+    filex = client.get_object(f"/files/{hash_calculated}")
+    with open("virustotal_report.txt", "w") as f:
+        f.write(f"Report for hash: {hash_calculated}\n")
+        f.write("[INFO] Detection ratios:\n")
+        f.write(f"  Malicious: {filex.last_analysis_stats['malicious']}\n")
+        f.write(f"  Suspicious: {filex.last_analysis_stats['suspicious']}\n")
+        f.write(f"  Undetected: {filex.last_analysis_stats['undetected']}\n")
 
 
 parser = argparse.ArgumentParser(description="VirusTotal hash checker")
@@ -35,6 +62,7 @@ group.add_argument("-u", "--url", help="url to check")
 parser.add_argument("-ht", "--hash_type", choices=["md5", "sha1", "sha256"], default="sha256",
                     help="Hash type to use for file (default: sha256)")
 parser.add_argument("-k", "--key", type=str, required=True, help="VirusTotal API key")
+parser.add_argument("-r", "--report", action="store_true", help="Outputs report of scanned file/hash")
 
 args = parser.parse_args()
 client = vt.Client(args.key)
@@ -45,6 +73,11 @@ if args.file:
     print(Fore.CYAN + f"[INFO] {args.hash_type} from file: {hash_value}")
 elif args.hash:
     hash_value = args.hash
+    if args.report:
+        try:
+            generate_report(hash_value)
+        except Exception as e:
+            pass
     print(Fore.CYAN + f"[INFO] Using provided hash: {hash_value}")
 elif args.url:
     hash_value = get_hash_from_url(args.url, args.hash_type)
